55974b6cd566b57c39f1e1d29097f7cbfa59145f
DumitruVartic <104945638+DumitruVartic@users.noreply.github.com.>
2024-12-07 22:53:15 +0200

    Add Docker support and refactor database configuration; implement database setup and replication management

--- app/database.py
+++ app/database.py
@@ -2,12 +2,10 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+asyncpg://postgres:yourpassword@localhost/datawarehouse"
+DATABASE_URL = "postgresql+asyncpg://postgres:yourpassword@postgres_primary/datawarehouse"
 
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
-SessionLocal = sessionmaker(
-    engine, class_=AsyncSession, autocommit=False, autoflush=False
-)
+SessionLocal = sessionmaker(engine, class_=AsyncSession, autocommit=False, autoflush=False)
 
 Base = declarative_base()
 
