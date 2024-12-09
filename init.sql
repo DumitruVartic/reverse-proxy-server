d38bd5433ba313371c5d8dfafdd674a903197281
Daniel Cojocaru <danielcojocaru@Daniels-MacBook-Air.local>
2024-12-09 14:45:21 +0200

    Init sql error on docker cloud fix

--- init.sql
+++ init.sql
@@ -1,9 +1,9 @@
--- Create replication role
-CREATE ROLE replicator WITH REPLICATION LOGIN PASSWORD 'replica_password';
+-- -- Create replication role
+-- CREATE ROLE replicator WITH REPLICATION LOGIN PASSWORD 'replica_password';
 
--- Allow replication connections from replica
-host replication replicator postgres_replica_ip/32 md5
+-- -- Allow replication connections from replica
+-- host replication replicator postgres_replica_ip/32 md5
 
--- Set up the WAL (Write-Ahead Log) level for replication
-ALTER SYSTEM SET wal_level = replica;
-SELECT pg_reload_conf();
+-- -- Set up the WAL (Write-Ahead Log) level for replication
+-- ALTER SYSTEM SET wal_level = replica;
+-- SELECT pg_reload_conf();
